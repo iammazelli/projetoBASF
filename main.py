6beb521160a7bd1e686b68a5700687a626015a38
Derick Mazelli Barbosa <67154185+iammazelli@users.noreply.github.com>
2025-05-29 12:13:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from getData import dataframeParaSimulacao
 from simulacao import SimuladorDaLimpeza
 
 df_medios = dataframeParaSimulacao() 
-simulador = SimuladorDaLimpeza(df_medios, db_name = "/home/derickoso/Area_de_Trabalho/Programming/Python/PEEBasf/planta_text.db")
+simulador = SimuladorDaLimpeza(df_medios, db_name = "planta_test.db")
 simulador.simular_periodo(30)
 dados_limpeza = simulador.ler_banco()
 print(dados_limpeza.head())

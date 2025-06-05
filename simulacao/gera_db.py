04356d36d91a2ea527f44c9d95978e7429f61fd1
Derick Mazelli Barbosa <derickmazellibarbosa@gmail.com>
2025-06-05 09:39:19 -0300

    Mudando umas coisinhas

--- simulacao/gera_db.py
+++ simulacao/gera_db.py
@@ -1,7 +1,10 @@
 from getData import dataframeParaSimulacao
 from simulation import SimuladorDaLimpeza
+import os
 
 df_medios = dataframeParaSimulacao() 
+if not os.path.exists("database/"):
+    os.makedirs("database/")
 simulador = SimuladorDaLimpeza(df_medios, db_name = "database/simulacao_planta.db")
 simulador.simular_periodo(350) #simulando 350 dias de atividade na planta
 dados_limpeza = simulador.ler_banco()
